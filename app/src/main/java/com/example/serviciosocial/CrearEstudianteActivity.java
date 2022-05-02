d68780a8d21b696ec6f7f22eaf83c0ca5b467115
jorgeromero17 <jorgeromero98@outlook.com>
2022-05-02 11:35:23 -0600

    pruebas iniciales de interfaces

---
+++ app/src/main/java/com/example/serviciosocial/CrearEstudianteActivity.java
@@ -0,0 +1,32 @@
+package com.example.serviciosocial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class CrearEstudianteActivity extends AppCompatActivity {
+
+    Spinner spinnerCarrera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_estudiante);
+
+        spinnerCarrera = findViewById(R.id.spinnerCarrera);
+
+        /*Carreras de prueba, estas deben venir de la DB*/
+        ArrayList<String> carreras = new ArrayList<String>();
+
+        carreras.add( "Ingenieria en Sistemas");
+        carreras.add( "Administrador con casco");
+        carreras.add( "Payasito");
+
+        ArrayAdapter<String>  adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,carreras);
+        spinnerCarrera.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/serviciosocial/MainActivity.java
+++ app/src/main/java/com/example/serviciosocial/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.serviciosocial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button botonPrueba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonPrueba = findViewById(R.id.button);
+        botonPrueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CrearEstudianteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
